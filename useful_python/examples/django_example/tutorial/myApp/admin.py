514b8a448561a7e6db4cf6c02a0c3f414a7a6be5
flash <puguiyu@gmail.com>
2019-10-23 21:26:13 +0800

    nice

--- useful_python/examples/django_example/tutorial/myApp/admin.py
+++ useful_python/examples/django_example/tutorial/myApp/admin.py
@@ -6,3 +6,9 @@ from django.contrib import admin
 from .models import Book
 
 admin.site.register(Book)
+
+
+
+
+
+
--- useful_python/examples/django_example/tutorial/myApp/apps.py
+++ useful_python/examples/django_example/tutorial/myApp/apps.py
@@ -1,5 +1,8 @@
 from django.apps import AppConfig
+from rest_framework import generics
 
 
 class MyappConfig(AppConfig):
     name = 'myApp'
+
+
