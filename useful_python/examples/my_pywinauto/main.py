1a3cd839cb5e704ea6094e088cdb7bdf54418415
732109610@qq.com <11111112>
2019-12-13 17:46:46 +0800

    自动化

--- useful_python/examples/my_pywinauto/main.py
+++ useful_python/examples/my_pywinauto/main.py
@@ -1,15 +1,18 @@
 import logging
 import time
-
+import logging.config
+import cv2
+import numpy as np
 import pyautogui as pyautogui
 import pywinauto
-from airtest.core.api import touch, exists
+from airtest.core.api import touch, exists, find_all
 from airtest.core.cv import Template
+from airtest.core.win import Windows
 from pywinauto.win32functions import GetSystemMetrics
 
 from useful_python.examples.my_pywinauto.tools import init_weixin, air_init
 
-logger = logging.getLogger(__name__)
+logger = logging.getLogger("main")
 
 X_MAX = GetSystemMetrics(0)
 Y_MAX = GetSystemMetrics(1)
@@ -19,10 +22,14 @@ X_BACK = -1
 Y_BACK = -1
 
 # 列表页向下移动像素
-SUBSCRIBE_DOWN_MOVE = 70
+SUBSCRIBE_DOWN_MOVE = int(Y_MAX * 0.064)
 SUBSCRIBE_RIGHT_MOVE = 700
 # 从下方点击订阅文章的初始位置
-SUBSCRIBE_DETAIL_START = 950
+SUBSCRIBE_DETAIL_START = int(Y_MAX * 0.88)
+# 详情页每上一步的距离
+SUBSCRIBE_DETAIL_UP = int(Y_MAX * 0.064)
+# 在详情页鼠标上移多少次
+SUBSCRIBE_DETAIL_UP_TIMES = 2
 
 IMG_PATH = 'imgs/'
 # 动作定义
@@ -31,9 +38,17 @@ CLICK_BACK = IMG_PATH + 'back.png'
 CLICK_NEW = IMG_PATH + 'new.png'
 SUBSCRIBE_TEXT = IMG_PATH + 'sub_text.png'
 MORE_TEXT = IMG_PATH + 'more.png'
+PIC_AM = IMG_PATH + 'am.png'
+PIC_PM = IMG_PATH + 'pm.png'
+PIC_NEW = IMG_PATH + 'new.png'
+# PIC_NEW = IMG_PATH + 'fake_new.png'
 
+# 图片序列名字
+img_index = 0
+bac_pos = False
 
 dlg = None
+
 
 def do_action(action):
     touch(Template(action))
@@ -46,12 +61,13 @@ def is_in_detail():
         return ret
     if ret[1] > 70 and ret[0] > X_MIDDLE:
         return False
+    pos = ret
     ret = exists(Template(MORE_TEXT))
     if not ret:
         return ret
     if ret[1] > 70 and ret[0] < X_MIDDLE:
         return False
-    return ret
+    return pos
 
 
 def set_back_position(x, y):
@@ -67,50 +83,140 @@ def click_detail_list():
     """
     x = X_MIDDLE
     y = SUBSCRIBE_DETAIL_START
-    for i in range(2):
+    for i in range(SUBSCRIBE_DETAIL_UP_TIMES):
         pywinauto.mouse.move(coords=(x, y))
         pywinauto.mouse.click(coords=(x, y))
-        time.sleep(2)
+        time.sleep(4)
         dlg.set_focus()
         pywinauto.mouse.move(coords=(x, y))
-        y -= 70
+        y -= SUBSCRIBE_DETAIL_UP
+        time.sleep(1)
+        logger.info('点击第%d篇文章' % i)
+
+
+def find_news():
+    ret_news = find_all(Template(PIC_NEW))
+    # ret_am = find_all(Template(PIC_AM))
+    # ret_pm = find_all(Template(PIC_PM))
+
+    if ret_news:
+        print(ret_news)
+
+        print(len(ret_news))
+    else:
+        ret_news = []
+
+    return ret_news
+
+
+# def run():
+#     while True:
+#         x, y = X_MIDDLE, 30
+#         for i in range(10):
+#             print(x, y)
+#             pywinauto.mouse.move(coords=(x, y + 70))
+#             pywinauto.mouse.click(coords=(x, y + 70))
+#
+#             click_detail_list()
+#
+#
+#             if is_in_detail():
+#                 set_back_position(*is_in_detail())
+#                 do_action(CLICK_BACK)
+#
+#             # time.sleep(2)
+#             x, y = x, y + 70
+#
+#         pywinauto.mouse.scroll(coords=(x, y), wheel_dist=-30)
+
+
+def scroll_to_top():
+    x, y = X_MIDDLE, Y_MIDDLE
+    for i in range(100):
+        pywinauto.mouse.scroll(coords=(x, y), wheel_dist=30)
+
+
+def screen_shot():
+    """
+
+    """
+
+    global img_index
+
+    img = pyautogui.screenshot(region=[0, 0, X_MAX, Y_MAX])  # x,y,w,h
+    img.save('screen_shot/' + str(img_index) + '.png')
+    img_index += 1
+    if img_index == 300:
+        img_index = 0
+
+    # img = cv2.cvtColor(np.asarray(img), cv2.COLOR_RGB2BGR)
+
+
+def run_new():
+    global bac_pos
+    scroll_to_top()
+    for i in range(10):
         time.sleep(2)
+        x, y = X_MIDDLE, Y_MIDDLE
 
+        rets = find_news()
 
+        logger.info('当前新消息的位置'+str(rets))
+        screen_shot()
 
-def run():
-    while True:
-        x, y = X_MIDDLE, 30
-        for i in range(10):
-            print(x, y)
-            pywinauto.mouse.move(coords=(x, y + 70))
-            pywinauto.mouse.click(coords=(x, y + 70))
+        for ret in rets:
+            result = ret.get('result', (0, 0))
+            pywinauto.mouse.move(coords=result)
+            time.sleep(1)
+
+            pywinauto.mouse.click(coords=result)
+
+            logger.info('点击进入公众号')
+            screen_shot()
+
 
             click_detail_list()
 
-
+            logger.info("开始判断是否在公众号页面,需不需要返回")
+            if not bac_pos:
+                bac_pos = is_in_detail()
             if is_in_detail():
-                set_back_position(*is_in_detail())
-                print("当前在某个公众号页面")
-                do_action(CLICK_BACK)
+                logger.info("当前在公众号详情页面，需要返回")
+                # set_back_position(*bac_pos)
+                # do_action(CLICK_BACK)
+                pywinauto.mouse.click(coords=bac_pos)
+            else:
+                # 没识别到返回页面，流程可能出错需要从进订阅号页面
+                do_action(CLICK_SUBSCRIBE)
+                logger.info("没识别到返回页面，流程可能出错需要从进订阅号页面")
+                screen_shot()
 
-            # time.sleep(2)
-            x, y = x, y + 70
+        time.sleep(1)
 
         pywinauto.mouse.scroll(coords=(x, y), wheel_dist=-30)
 
 
 if __name__ == "__main__":
 
+    logging.config.fileConfig('logging.cfg')
+
+    # logger.debug("debug")
+    # logger.info("info")
+    # logger.error("error")
 
     dlg = init_weixin()
     uuid = dlg.handle
     air_init(uuid)
     do_action(CLICK_SUBSCRIBE)
 
+    screen_shot()
+
     pywinauto.mouse.move(coords=(X_MIDDLE, 0))
 
-    run()
-
-
-
+    while True:
+        try:
+            run_new()
+        except Exception as e:
+            print(e)
+            logger.error(e)
+            time.sleep(10)
--- useful_python/examples_jupyter/docs/appium.md
+++ useful_python/examples_jupyter/docs/appium.md
@@ -8,5 +8,28 @@ pip install -i https://pypi.tuna.tsinghua.edu.cn/simple Appium-Python-Client
 adb connect 127.0.0.1:62001
 
 
-# 
+# 查看安卓当前activity
 adb shell dumpsys window windows | grep mFocusedApp
+
+
+
+# appium配置参数
+    ```
+    {
+      "platformName": "Android",
+      //安卓版本号
+      "platformVersion": "6",
+      //设备名，安卓上可以随便填写
+      "deviceName": "127.0.0.1:62001",
+      //以下2个分别是app包名和启动入口
+      "appPackage": "com.android.settings",
+      "appActivity": "com.oppo.settings.SettingsActivity",
+      //每回启动是重置命令
+      "noReset": true,
+      //每回启动是否需要安装一些服务
+      "skipServerInstallation": false,
+      "skipDeviceInitialization": false
+    }
+    ```
+
+
