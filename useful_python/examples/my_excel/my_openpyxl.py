674e676277beefbb43a801365c4cd50f8d4f67fb
732109610@qq.com <11111112>
2020-02-10 11:01:34 +0800

    openpy读取

---
+++ useful_python/examples/my_excel/my_openpyxl.py
@@ -0,0 +1,28 @@
+from openpyxl import load_workbook
+
+
+def read_line():
+    wb = load_workbook("../../examples_jupyter/docs/demo.xlsx")
+    ws = wb.active
+    for row in ws.rows:
+        for cell in row:
+            print(cell.value)
+
+
+
+
+
+
+if __name__ == "__main__":
+
+    read_line()
+
+
+
+
+
+
+
+
+
+
