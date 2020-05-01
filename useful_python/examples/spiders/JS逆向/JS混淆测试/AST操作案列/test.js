86a77ce69a80713ac83b4cfba1f5b2cca4388d02
pgy <732109610@qq.com>
2020-05-01 11:43:13 +0800

    hope

---
+++ useful_python/examples/spiders/JS逆向/JS混淆测试/AST操作案列/test.js
@@ -0,0 +1,10 @@
+
+
+a = [1,1,1,1]
+
+const b = a.find(x => {return x>2});
+
+
+if(b){
+    console.log("ok")
+}
--- useful_python/examples/spiders/JS逆向/案例/BOSS直聘/main.js
+++ useful_python/examples/spiders/JS逆向/案例/BOSS直聘/main.js
@@ -1,6 +1,4 @@
-
-
-const { decodeFun, decodeFunName} = require('./module');
+const {decodeFun, decodeFunName} = require('./module');
 // 把js源码转成语法树
 const parser = require("@babel/parser");
 // 遍历语法树中的节点
@@ -19,13 +17,13 @@ var js = fs.readFileSync("./security.js", {
 let ast = parser.parse(js);
 
 
-traverse(ast,{
-    CallExpression(path){
+traverse(ast, {
+    CallExpression(path) {
         let node = path.node
-        if(!t.isIdentifier(node.callee) && node.callee.name !== decodeFunName)
+        if (!t.isIdentifier(node.callee) && node.callee.name !== decodeFunName)
             return
 
-        if(!t.isStringLiteral(node.arguments[0]))
+        if (!t.isStringLiteral(node.arguments[0]))
             return
 
         let argument = node.arguments[0].value
@@ -40,7 +38,6 @@ traverse(ast,{
 });
 
 
-
 // let { code } = generator(ast)
 // code = code.replace(/!!\[\]/g, 'true').replace(/!\[\]/g, 'false')
 // fs.writeFile('./result1.js', code, function (err) {
@@ -51,14 +48,14 @@ traverse(ast,{
 
 
 traverse(ast, {
-    VariableDeclaration(path){
+    VariableDeclaration(path) {
         let node = path.node
 
         // 第一步 找到var _0xb20cd3 = {};这样的变量
-        if(node.declarations.length !== 1)
+        if (node.declarations.length !== 1)
             return;
         let declara = node.declarations[0];
-        if(!t.isObjectExpression(declara.init) || declara.init.properties.length !== 0)
+        if (!t.isObjectExpression(declara.init) || declara.init.properties.length !== 0)
             return;
         // 获取到_0xb20cd3的变量名字
         let object_name = declara.id.name;
@@ -68,41 +65,34 @@ traverse(ast, {
         // let fun_bodys = start_fun_path.node.body.body;
 
 
-
-
         // 遍历整个函数，找到对应的语句直接替换
         // console.log(start_fun_path)
-        if(start_fun_path){
+        if (start_fun_path) {
             start_fun_path.traverse({
-                ExpressionStatement(path){
-                let node = path.node
-                if(!t.isAssignmentExpression(node.expression))
-                    return
+                ExpressionStatement(path) {
+                    let node = path.node
+                    if (!t.isAssignmentExpression(node.expression))
+                        return
 
-                    // console.log("okkk")
-
-                let exp = node.expression
-                    if(t.isFunctionExpression(exp.right)){
+                    let exp = node.expression
+                    if (t.isFunctionExpression(exp.right)) {
 
 
-                        if(!t.isMemberExpression(exp.left) || !t.isFunctionExpression(exp.right) || !t.isIdentifier(exp.left.object) || exp.left.object.name !== object_name)
+                        if (!t.isMemberExpression(exp.left) || !t.isFunctionExpression(exp.right) || !t.isIdentifier(exp.left.object) || exp.left.object.name !== object_name)
                             return
                         // 当前节点确定为一个函数赋值表达式,确定替换体
                         let property_name = exp.left.property.value;
                         let retStmt = exp.right.body.body[0] // 替换体
 
 
-
-
-
                         // 将满足条件的语句替换
                         start_fun_path.traverse({
-                             CallExpression: function (_path) {
+                            CallExpression: function (_path) {
 
                                 if (!t.isMemberExpression(_path.node.callee)) return
 
-                                 if(_path.node.callee.property.value !== property_name)
-                                     return
+                                if (_path.node.callee.property.value !== property_name)
+                                    return
                                 let node = _path.node.callee
                                 if (!t.isIdentifier(node.object)) return
                                 let args = _path.node.arguments // 调用传入的参数
@@ -121,33 +111,40 @@ traverse(ast, {
                         })
                     }
 
-                    if(t.isMemberExpression(exp.left)){
-                        if(!t.isMemberExpression(exp.left) || !t.isStringLiteral(exp.right) || exp.left.object.name !== object_name)
-                        return
+                    if (t.isStringLiteral(exp.right)) {
+                        if (!t.isMemberExpression(exp.left) || !t.isStringLiteral(exp.right) || exp.left.object.name !== object_name)
+                            return
 
-                    // 当前节点确定为一个函数赋值表达式,确定替换体
-                    //     console.log(exp.right.property)
-                    let property_name = exp.left.property.value;
-                    let retStmt = exp.right.value // 替换体
+                        // 当前节点确定为一个函数赋值表达式,确定替换体
+                        //     console.log(exp.right.property)
+                        let property_name = exp.left.property.value;
+                        let retStmt = exp.right.value // 替换体
 
-                     start_fun_path.traverse({
-                         MemberExpression(_path){
-                             let node = path.node
+                        start_fun_path.traverse({
+                            MemberExpression(_path) {
+                                let node = _path.node
 
-                            if(!t.isStringLiteral(node.property) || t.property.value !== property_name)
-                                return
+                                let _parent = _path.parentPath
+                                if(t.isAssignmentExpression(_parent.node))
+                                    return
 
-                             _path.replaceWith(t.stringLiteral(retStmt))
-                         }
-                     })
+                                if(path === _path)
+                                    return
+
+                                if (!t.isStringLiteral(node.property) || node.property.value !== property_name)
+                                    return
+
+                                _path.replaceWith(t.stringLiteral(retStmt))
+                            }
+                        })
 
                     }
 
 
-                path.remove()
-            },
+                    path.remove()
+                },
 
-        })
+            })
         }
 
 
@@ -159,7 +156,7 @@ traverse(ast, {
 })
 
 
-let { code } = generator(ast)
+let {code} = generator(ast)
 code = code.replace(/!!\[\]/g, 'true').replace(/!\[\]/g, 'false')
 fs.writeFile('./result2.js', code, function (err) {
     if (err) console.error(err);
